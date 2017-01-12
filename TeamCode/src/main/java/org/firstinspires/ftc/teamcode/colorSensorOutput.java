91b2a2568e6c6748ec1fc51013d8290ac74b9507
Aidan Boyer <Aidan Boyer>
2017-01-12 12:37:24 -0500

    programming

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSensorOutput.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by agb on 1/11/2017.
+ */
+
+public class colorSensorOutput {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveFixedDistanceEncoders.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by agb on 1/10/2017.
+ */
+
+public class driveFixedDistanceEncoders {
+}
